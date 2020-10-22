87dd46c2bbebd7f6f3a283df2fb2172ad0522971
Your Name <you@example.com>
2020-10-22 22:15:39 +0800

    add nodejs package

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'app')));
+app.get('*', function(req, res) {
+res.sendFile(path.join(__dirname, 'app', 'index.html'));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
